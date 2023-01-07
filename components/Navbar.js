f3213ca888a3ebd93bc205d22470278cd8964d39
Pum <28806828+PumPum7@users.noreply.github.com>
2023-01-07 11:29:14 +0000

    update navbar links and text

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,54 +26,63 @@ const Navbar = () => {
         >
           <span class="sr-only">Open main menu</span>
           <svg
-            class="w-6 h-6"
+            className="w-6 h-6"
             aria-hidden="true"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="flex flex-col items-center	p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:dark:bg-orange-400">
+          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:dark:bg-orange-400">
             <li>
               <a
                 href="https://www.bonkcoin.com/"
                 target="_blank"
-                class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
                 aria-current="page"
               >
-                BONK COIN
+                BONK WEBSITE
               </a>
             </li>
             <li>
               <a
                 href="https://www.bonkcoin.com/one-pager"
                 target="_blank"
-                class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
               >
                 ONE PAGER
               </a>
             </li>
             <li>
               <a
-                href="https://www.bonkcoin.com/links"
+                href="https://www.bonkcoin.com/integrations"
                 target="_blank"
-                class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
               >
-                USEFUL LINKS
+                INTEGRATIONS
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.bonkcoin.com/bonkpaper"
+                target="_blank"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
+              >
+                BONK PAPER
               </a>
             </li>
             <li>
               <a
                 href="https://jup.ag/swap/wSOL-Bonk"
                 target="_blank"
-                class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
+                className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0"
               >
                 BUY $BONK
               </a>
